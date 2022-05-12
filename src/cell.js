173ae82ce0b32c270443fd47627ad0620ef8d1d8
Robert Parker <diaboloshogunate@gmail.com>
2022-05-11 18:35:56 -0700

    remove position from cell

--- src/cell.js
+++ src/cell.js
@@ -1,14 +1,4 @@
 class Cell {
-    #position
-
-    constructor(position) {
-        this.position = position
-    }
-
-    get position() {
-        return this.#position
-    }
-
     onPlayerCollision(player) {
         if(!player instanceof Player)
             throw `onPlayerCollision requires the player is passed in as an argument`
