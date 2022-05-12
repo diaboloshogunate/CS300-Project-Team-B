6a358f690d8b4fde178544e0ee10fba81a3f09a5
Robert Parker <diaboloshogunate@gmail.com>
2022-05-11 18:36:20 -0700

    update player test

--- tests/player.js
+++ tests/player.js
@@ -1,6 +1,6 @@
 QUnit.module('player.js', function() {
     QUnit.test('move 0', assert => {
-        let player = new Player(new Vector(5,5), 1000, 100)
+        let player = new Player(new Vector(5,5), 1000, 100, 1000, new Map(10))
         player.move(0, 2)
 
         assert.equal(player.position.x, 7, 'moved 2 in x direction')
@@ -10,8 +10,8 @@ QUnit.module('player.js', function() {
     });
 
     QUnit.test('move 90', assert => {
-        let player = new Player(new Vector(5,5), 1000, 100)
-        player.move(90.0 * (Math.PI / 180.0), 3)
+        let player = new Player(new Vector(5,5), 1000, 100, 1000, new Map(10))
+        player.move(degreeToRad(90), 3)
 
         assert.equal(player.position.x, 5, 'moved 2 in x direction')
         assert.equal(player.position.y, 8, 'moved 0 in y direction')
@@ -20,8 +20,8 @@ QUnit.module('player.js', function() {
     });
 
     QUnit.test('move 180', assert => {
-        let player = new Player(new Vector(5,5), 1000, 100)
-        player.move(180.0 * (Math.PI / 180.0), 3)
+        let player = new Player(new Vector(5,5), 1000, 100, 1000, new Map(10))
+        player.move(degreeToRad(180), 3)
 
         assert.equal(player.position.x, 2, 'moved 2 in x direction')
         assert.equal(player.position.y, 5, 'moved 0 in y direction')
@@ -30,8 +30,8 @@ QUnit.module('player.js', function() {
     });
 
     QUnit.test('move 270', assert => {
-        let player = new Player(new Vector(5,5), 1000, 100)
-        player.move(270.0 * (Math.PI / 180.0), 3)
+        let player = new Player(new Vector(5,5), 1000, 100, 1000, new Map(10))
+        player.move(degreeToRad(270), 3)
 
         assert.equal(player.position.x, 5, 'moved 2 in x direction')
         assert.equal(player.position.y, 2, 'moved 0 in y direction')
