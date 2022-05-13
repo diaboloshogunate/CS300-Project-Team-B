0a2dbec56a69539ac359515805353a49d5da97a5
Robert Parker <diaboloshogunate@gmail.com>
2022-05-12 19:13:37 -0700

    allow cells to return background color for visuals

--- src/cell.js
+++ src/cell.js
@@ -9,6 +9,14 @@ class Cell {
         this.#isHidden = value
     }
 
+    get backgroundColor() {
+        return `#000`
+    }
+
+    playerChoices(player) {
+        // return array of string actions and the callable they will run if the player choose it?
+    }
+
     onPlayerCollision(player) {
         if(!player instanceof Player)
             throw `onPlayerCollision requires the player is passed in as an argument`
