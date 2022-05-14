983d70af03ee7e570a0112cf9af18613b0155110
Robert Parker <diaboloshogunate@gmail.com>
2022-05-14 16:51:05 -0700

    add js docs and use util validate methods

--- src/player.js
+++ src/player.js
@@ -1,91 +1,159 @@
+/**
+ * class for a player
+ */
 class Player {
-    #position
-    #energy
-    #energyCapacity
-    #supplies
-    #credits
-    #map
+    position
+    energy
+    energyCapacity
+    supplies
+    credits
+    map
 
+    /**
+     * create a player
+     * @param {Vector} position starting position
+     * @param {number} energy starting amount of energy
+     * @param {number} energyCapacity maximum amount of energy the player can have
+     * @param {number} supplies starting amount of supplis
+     * @param {number} credits starting amount of credits
+     * @param {Cell[][]} map map
+     */
     constructor(position, energy, energyCapacity, supplies, credits, map) {
-        this.#position       = position || new Vector(0,0)
-        this.#energy         = energy || 1000
-        this.#energyCapacity = energyCapacity || 1000
-        this.#supplies       = supplies || 100
-        this.#credits        = credits || 1000
+        this.position       = position || new Vector(0,0)
+        this.energy         = energy || 1000
+        this.energyCapacity = energyCapacity || 1000
+        this.supplies       = supplies || 100
+        this.credits        = credits || 1000
         this.map             = map
+        this.position
     }
 
+    /**
+     * get current position
+     * @returns {Vector}
+     */
     get position() {
-        return this.#position
+        return this.position
     }
 
+    /**
+     * set current position, reveal it in map and trigger cell collision
+     * position is clamped to the map size
+     * @param value
+     */
     set position(value) {
-        if(!value instanceof Vector)
-            throw `Invalid position. Position must be an instance of value. Provided ${value.name}`
+        validateType(value, Vector.name)
+        value.x = clamp(0,1,this.map.size - 1)
+        value.x = clamp(0,1,this.map.size - 1)
 
-        this.#position = value
-        this.#map.revealPosition(this.position)
-        this.#map.triggerPlayerCollision(this)
+        this.position = value
+        this.map.revealPosition(value)
+        this.map.triggerPlayerCollision(this)
     }
 
+    /**
+     * get the current energy level
+     * @returns {number}
+     */
     get energy() {
-        return this.#energy
+        return this.energy
     }
 
+    /**
+     * get energy
+     * @param {number} value
+     */
     set energy(value) {
-        if(!Number.isSafeInteger(value))
-            throw `Invalid energy. Must be a safe integer. Provided ${value}`
+        validateSafeInt(value)
 
-        this.#energy = clamp(value, 0, this.energyCapacity)
+        this.energy = clamp(value, 0, this.energyCapacity)
     }
 
+    /**
+     * get energy capacity
+     * @returns {number}
+     */
     get energyCapacity() {
-        return this.#energyCapacity
+        return this.energyCapacity
     }
 
+    /**
+     * set current energy capacity
+     * @param {number} value
+     */
     set energyCapacity(value) {
-        if(!Number.isSafeInteger(value))
-            throw `Invalid energy. Must be a safe integer. Provided ${value}`
+        validateSafeInt(value)
 
-        this.#energyCapacity = value
+        this.energyCapacity = value
     }
 
+    /**
+     * get supplies
+     * @returns {number} supplies
+     */
     get supplies() {
-        return this.#supplies
+        return this.supplies
     }
 
+    /**
+     * set supplies
+     * @param {number} value
+     */
     set supplies(value) {
-        if(!Number.isSafeInteger(value) || value < 0 || value > 100)
-            throw `Invalid supplies. Must be a safe integer between 0-100 (inclusive). Provided ${value}`
+        validateSafeInt(value)
+        value = clamp(value, 0, 100)
 
-        this.#supplies = value
+        this.supplies = value
+
+        if(this.supplies === 0)
+            throw `Ran out of supplies. You lose the game`
     }
 
+    /**
+     * get credits
+     * @returns {number}
+     */
     get credits() {
-        return this.#credits
+        return this.credits
     }
 
+    /**
+     * set credits
+     * @param {number} value
+     */
     set credits(value) {
-        if(!Number.isSafeInteger(value) || value < 0 || value > 100)
-            throw `Invalid credits. Must be a safe integer between 0-100 (inclusive). Provided ${value}`
+        validateSafeInt(value)
 
-        this.#credits = value
+        this.credits = value
     }
 
+    /**
+     * get map
+     * @returns {Map}
+     */
     get map() {
-        return this.#map
+        return this.map
     }
 
+    /**
+     * sets the map
+     * @param {Map} value
+     */
     set map(value) {
-        if(!value instanceof Map)
-            throw `Invalid type. Expected Map. Giver ${value.name}`
+        validateType(value, Map.name)
 
-        this.#map = value
+        this.map = value
     }
 
+    /**
+     * move the player in a direction for a given distance
+     * player will move one unit at a time until it reaches the final point
+     * @param {number} direction angle in rads such as Math.PI
+     * @param {number} magnitude distance in units
+     */
     move(direction, magnitude) {
-        this.#validateDirection(direction)
-        this.#validateMagnitude(magnitude)
+        validateFloat(direction)
+        validateSafeInt(magnitude)
 
 
         // move one unit at a time so collisions can trigger
@@ -96,7 +164,7 @@ class Player {
             let movement = polarToCoordinate(direction, distanceTraveled)
             let nextPosition = new Vector(startingPosition.x + movement.x, startingPosition.y + movement.y)
 
-            if(!this.#map.is(nextPosition)) {
+            if(!this.map.is(nextPosition)) {
                 const wormhole = new Wormhole()
                 wormhole.onPlayerCollision(this)
                 break
@@ -107,40 +175,30 @@ class Player {
         }
 
         this.supplies = this.supplies - 2
-        this.#validateSupplies()
     }
 
-    scan(position, distance) {
-        const startX = clamp(position.x - distance, 0, this.map.size - 1)
-        const endX   = clamp(position.x + distance, 0, this.map.size - 1)
-        const startY = clamp(position.y - distance, 0, this.map.size - 1)
-        const endY   = clamp(position.y + distance, 0, this.map.size - 1)
+    /**
+     * scan the map and reveal points
+     * @param {Vector} position position to center the scan at
+     * @param {number} radius distance of the scan
+     */
+    scan(position, radius) {
+        const startX = clamp(position.x - radius, 0, this.map.size - 1)
+        const endX   = clamp(position.x + radius, 0, this.map.size - 1)
+        const startY = clamp(position.y - radius, 0, this.map.size - 1)
+        const endY   = clamp(position.y + radius, 0, this.map.size - 1)
 
         for(let i = startX; i <= endX; i++) {
             for(let j = startY; j <= endY; j++) {
-                this.#map.revealPosition(new Vector(i, j))
+                this.map.revealPosition(new Vector(i, j))
             }
         }
     }
 
-    #validateDirection(direction) {
-        let parsedDirection = parseFloat(direction)
-
-        if(isNaN(parsedDirection))
-            throw `Invalid magnitude. Must be a valid float. Provided ${direction}`
-    }
-
-    #validateMagnitude(magnitude)
-    {
-        if(!Number.isSafeInteger(magnitude))
-            throw `Invalid magnitude. Must be a safe integer. Provided ${magnitude}`
-    }
-
-    #validateSupplies(){
-        if(this.#supplies <= 0)
-            throw `Ran out of supplies. You lose the game`
-    }
-
+    /**
+     * get html representation of the player
+     * @returns {string} table with player stats
+     */
     toString() {
         return`<table class="table table-sm table-hover table-borderless">
             <tbody>
