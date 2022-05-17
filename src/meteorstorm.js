ba2c6c72f953a95d4f03ecb3818e51b800692b30
ELondonJ <ljoseph@pdx.edu>
2022-05-16 18:59:39 -0700

    fixed message bug

--- src/meteorstorm.js
+++ src/meteorstorm.js
@@ -2,6 +2,6 @@ class MeteorStorm extends Cell {
     onPlayerCollision(player) {
         super.onPlayerCollision(player);
         // damage ship
-        player.message = "Collsion with meteor storm. Ship is damaged"
+        player.messages= "Collsion with meteor storm. Ship is damaged"
     }
 }
